12f14e425e147210248176f99849097df1a9c9ee
Nguyen Minh Khoa <89793291+khoauuu123@users.noreply.github.com>
2025-08-04 11:28:10 +0700

    Add files via upload

---
+++ UPDATE_BIN_FILE_ESP32/UPDATE_BIN_FILE_ESP32/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace UPDATE_BIN_FILE_ESP32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_com = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tb_terminal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_HexFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox_com
+            // 
+            this.comboBox_com.FormattingEnabled = true;
+            this.comboBox_com.Location = new System.Drawing.Point(12, 70);
+            this.comboBox_com.Name = "comboBox_com";
+            this.comboBox_com.Size = new System.Drawing.Size(170, 21);
+            this.comboBox_com.TabIndex = 16;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(107, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Upload";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Com load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tb_terminal
+            // 
+            this.tb_terminal.Location = new System.Drawing.Point(200, 23);
+            this.tb_terminal.Multiline = true;
+            this.tb_terminal.Name = "tb_terminal";
+            this.tb_terminal.Size = new System.Drawing.Size(328, 97);
+            this.tb_terminal.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "COM";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(149, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Bin file";
+            // 
+            // tb_HexFilePath
+            // 
+            this.tb_HexFilePath.Location = new System.Drawing.Point(12, 24);
+            this.tb_HexFilePath.Name = "tb_HexFilePath";
+            this.tb_HexFilePath.Size = new System.Drawing.Size(131, 20);
+            this.tb_HexFilePath.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(543, 137);
+            this.Controls.Add(this.comboBox_com);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tb_terminal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_HexFilePath);
+            this.Name = "Form1";
+            this.Text = "UPDATE ESP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_com;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tb_terminal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_HexFilePath;
+    }
+}
+
---
+++ UPDATE_BIN_FILE_ESP32/UPDATE_BIN_FILE_ESP32/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UPDATE_BIN_FILE_ESP32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                comboBox_com.DataSource = SerialPort.GetPortNames();
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi trong MessageBox
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                comboBox_com.DataSource = SerialPort.GetPortNames();
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi trong MessageBox
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog
+                {
+                    Filter = "Firmware File (*.bin)|*.bin",
+                    Title = "Chọn file firmware"
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    tb_HexFilePath.Text = openFileDialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi trong MessageBox
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string binFilePath = tb_HexFilePath.Text;
+                string comPort = comboBox_com.Text;
+
+                if (string.IsNullOrEmpty(binFilePath) || string.IsNullOrEmpty(comPort))
+                {
+                    MessageBox.Show("Please select a HEX file and COM port.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Hộp thoại xác nhận từ người dùng
+                DialogResult result = MessageBox.Show(
+                    "Bạn có chắc chắn muốn cập nhật firmware không?",
+                    "Xác nhận",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    // Bắt đầu quá trình upload
+                    UploadFirmware(comPort, binFilePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi trong MessageBox
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UploadFirmware(string comPort, string firmwarePath)
+        {
+            string pythonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "esptool.exe");
+            string bootAppPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "boot_app0.bin");
+
+            string arguments;
+
+            arguments = $"--chip esp32 --port {comPort} --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x10000 \"{firmwarePath}\" 0xe000 \"{bootAppPath}\"";
+
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = pythonPath,
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+
+            if (process.ExitCode == 0)
+            {
+                tb_terminal.Text = "Upload successful:\n" + output;
+            }
+            else
+            {
+                tb_terminal.Text = "Upload failed:\n" + error;
+            }
+        }
+    }
+}
